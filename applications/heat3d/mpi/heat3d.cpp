5c1076577aac64a65f48779b1495aa624a349226
Christian Nicole Avans <cnicoleavans@live.com>
2024-06-10 15:53:50 -0600

    Update heat3d.cpp for test

--- applications/heat3d/mpi/heat3d.cpp
+++ applications/heat3d/mpi/heat3d.cpp
@@ -86,8 +86,8 @@ struct CommHelper {
   template <class ViewType>
   void isend_irecv(int partner, ViewType send_buffer, ViewType recv_buffer,
                    MPI_Request* request_send, MPI_Request* request_recv) {
-    KokkosComm::irecv(recv_buffer, partner, tag, comm, request_recv); //TODO
-    KokkosComm::isend(send_buffer, partner, tag, comm, request_send); //TODO
+    KokkosComm::irecv(recv_buffer, partner, 1, comm, request_recv); //TODO
+    KokkosComm::isend(send_buffer, partner, 1, comm, request_send); //TODO
   }
 };
 
